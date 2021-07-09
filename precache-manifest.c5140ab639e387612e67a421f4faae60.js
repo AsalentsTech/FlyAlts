bec0b408bd38bd5e895d42296e7d936b45b8c325
AsalentsTech <unknownasalent432@gmail.com>
2021-07-09 07:45:42 -0500

    Updates

---
+++ precache-manifest.c5140ab639e387612e67a421f4faae60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897531c66054ffaea2623a52e6648e2c",
+    "url": "/Give-A-Way-ReactJS/index.html"
+  },
+  {
+    "revision": "c6b50850349ed3840486",
+    "url": "/Give-A-Way-ReactJS/static/css/main.962fa208.chunk.css"
+  },
+  {
+    "revision": "5bd4c5fdc9399cd322c8",
+    "url": "/Give-A-Way-ReactJS/static/js/2.02e4186a.chunk.js"
+  },
+  {
+    "revision": "4c843cd99dd6e16a9fa7ab024359f05e",
+    "url": "/Give-A-Way-ReactJS/static/js/2.02e4186a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b50850349ed3840486",
+    "url": "/Give-A-Way-ReactJS/static/js/main.d74a5c05.chunk.js"
+  },
+  {
+    "revision": "73e7f1c33cdb6ccadda8",
+    "url": "/Give-A-Way-ReactJS/static/js/runtime-main.c4f5d91e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e45da21a36265bdc683e359ed5e25ab8.js"
+  "/Give-A-Way-ReactJS/precache-manifest.c5140ab639e387612e67a421f4faae60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Give-A-Way-ReactJS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
