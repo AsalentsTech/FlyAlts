e7368a394e80d04544dd3093c962577a6afa6ce8
AsalentsTech <unknownasalent432@gmail.com>
2021-07-09 06:16:29 -0500

    Updates

---
+++ precache-manifest.911cbd06590d603a8d3db95b2132cd44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d58b56b340b2ce69165b204902908b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67847f5dc4e531539193",
+    "url": "/static/css/main.962fa208.chunk.css"
+  },
+  {
+    "revision": "8d9548bee721e9bbb6bf",
+    "url": "/static/js/2.2247830e.chunk.js"
+  },
+  {
+    "revision": "5cd1ab69b26d62b4ecc1a413b63695e9",
+    "url": "/static/js/2.2247830e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67847f5dc4e531539193",
+    "url": "/static/js/main.af00358f.chunk.js"
+  },
+  {
+    "revision": "db4713b42f5bf22e0d49",
+    "url": "/static/js/runtime-main.d2a9331b.js"
+  }
+]);
