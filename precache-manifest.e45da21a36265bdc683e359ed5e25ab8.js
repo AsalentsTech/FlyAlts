80991e137d20a19ac9b150efd9e178330d8202d5
AsalentsTech <unknownasalent432@gmail.com>
2021-07-09 08:31:10 -0500

    Updates

---
+++ precache-manifest.e45da21a36265bdc683e359ed5e25ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4883ecc11f325dcd764dfed1c41728c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b50850349ed3840486",
+    "url": "/static/css/main.962fa208.chunk.css"
+  },
+  {
+    "revision": "5bd4c5fdc9399cd322c8",
+    "url": "/static/js/2.02e4186a.chunk.js"
+  },
+  {
+    "revision": "4c843cd99dd6e16a9fa7ab024359f05e",
+    "url": "/static/js/2.02e4186a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b50850349ed3840486",
+    "url": "/static/js/main.d74a5c05.chunk.js"
+  },
+  {
+    "revision": "f51565d310c4b24511c7",
+    "url": "/static/js/runtime-main.a95d1374.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FlyAlts/precache-manifest.887b425870768753e90a45af35e1f4a9.js"
+  "/precache-manifest.e45da21a36265bdc683e359ed5e25ab8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FlyAlts/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
